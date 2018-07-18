77fe2656b7936c86b82496e0844d2f777ee86e75
Pascal Laptop <krafft.pascal@gmail.com>
2018-07-18 19:57:16 +0200

    fixed last commit

--- src/SearchPage.js
+++ src/SearchPage.js
@@ -1,11 +1,7 @@
-import React, {
-  Component
-} from "react";
+import React, { Component } from "react";
 import BooksSlectedAfterShelf from "./BooksSlectedAfterShelf";
 import * as BooksAPI from "./BooksAPI.js";
-import {
-  Link
-} from "react-router-dom";
+import { Link } from "react-router-dom";
 
 export default class SearchPage extends Component {
   state = {
@@ -14,99 +10,65 @@ export default class SearchPage extends Component {
   };
 
   handleChange = event => {
-    if (event.target.value === '') {
-      this.setState({
-        books: []
-      })
+    if (event.target.value === "") {
+      this.setState({ books: [] });
     }
-    this.setState({
-      query: event.target.value
-    })
-
-
+    this.setState({ query: event.target.value });
   };
 
   searchForBooks = searchedbooks => {
     if (JSON.stringify(searchedbooks) !== JSON.stringify(this.state.books)) {
       this.setState({
         books: searchedbooks
-      })
+      });
     }
-
-  }
+  };
 
   render() {
-      const {
-        query
-      } = this.state;
-      if (query) {
+    const { query } = this.state;
+    if (query) {
+      BooksAPI.search(query).then(books => {
+        this.searchForBooks(books);
+      });
+    }
 
-        BooksAPI.search(query).then(books => {
-          this.searchForBooks(books)
+    return (
+      <div>
+        <div className="search-books">
+          <div className="search-books-bar">
+            <Link to="/">
+              <a className="close-search">Close</a>
+            </Link>
 
-        });
+            <div className="search-books-input-wrapper">
+              {/*
+      NOTES: The search from BooksAPI is limited to a particular set of search terms.
+      You can find these search terms here:
+      https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
 
-      }
+      However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
+      you don't find a specific author or title. Every search is limited by search terms.
+    */}
 
-
-
-
-      return ( <
-        div >
-        <
-        div className = "search-books" >
-        <
-        div className = "search-books-bar" >
-        <
-        Link to = "/" >
-        <
-        a className = "close-search" > Close < /a> <
-        /Link>
-
-        <
-        div className = "search-books-input-wrapper" > {
-          /*
-                NOTES: The search from BooksAPI is limited to a particular set of search terms.
-                You can find these search terms here:
-                https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-
-                However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-                you don't find a specific author or title. Every search is limited by search terms.
-              */
-        }
-
-        { /* get input string from form */ } <
-        input id = "search-books"
-        type = "text"
-        placeholder = "Search by title or author"
-        value = {
-          query
-        }
-        onChange = {
-          this.handleChange
-        }
-        /> <
-        /div> <
-        /div> <
-        div className = "search-books-results" >
-        <
-        ol className = "books-grid" >
-
-
-        {
-          this.state.books.length > 0 && ( <
-            BooksSlectedAfterShelf books = {
-              this.state.books
-            }
-            />
-          ) || ( < p > no search results < /p>)}
-
-
-            <
-            /ol> <
-            /div> <
-            /div> <
-            /div>
-          );
-        }
-      }
+              {/* get input string from form */}
+              <input
+                id="search-books"
+                type="text"
+                placeholder="Search by title or author"
+                value={query}
+                onChange={this.handleChange}
+              />
+            </div>
+          </div>
+          <div className="search-books-results">
+            <ol className="books-grid">
+              {(this.state.books.length > 0 && (
+                <BooksSlectedAfterShelf books={this.state.books} />
+              )) || <p> no search results </p>}
+            </ol>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
