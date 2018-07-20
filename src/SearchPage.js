37b1a12fe5e3e85a38c0cbc7e638951b0ebaa602
Pascal Laptop <krafft.pascal@gmail.com>
2018-07-20 10:24:04 +0200

    fixed infinite loop of rerender

--- src/SearchPage.js
+++ src/SearchPage.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
-import BooksSlectedAfterShelf from "./BooksSlectedAfterShelf";
-import * as BooksAPI from "./BooksAPI.js";
+import { Books } from "./BooksSlectedAfterShelf";
+import * as BooksAPI from "./BooksAPI";
 import { Link } from "react-router-dom";
 
 export default class SearchPage extends Component {
@@ -10,64 +10,87 @@ export default class SearchPage extends Component {
   };
 
   handleChange = event => {
-    if (event.target.value === "") {
-      this.setState({ books: [] });
-    }
-    this.setState({ query: event.target.value });
-  };
+    let booksInShelf = [];
+    let searchedBooksShelfed = [];
+    let count = true;
 
-  searchForBooks = searchedbooks => {
-    if (JSON.stringify(searchedbooks) !== JSON.stringify(this.state.books)) {
+    this.setState({
+      query: event.target.value
+    });
+    if (event.target.value !== "") {
+      BooksAPI.search(event.target.value).then(searchedbooks => {
+        // check if a book is already in a shelf and add this property to
+        //  the searched books in order to display this property
+        if (typeof searchedbooks.error === "undefined") {
+          BooksAPI.getAll().then(requestBooks => {
+            booksInShelf = requestBooks;
+            booksInShelf.forEach(elShelf => {
+              searchedbooks.forEach(elSearch => {
+                if (count) {
+                  if (elShelf.id === elSearch.id) {
+                    elSearch.shelf = elShelf.shelf;
+                    searchedBooksShelfed.push(elSearch);
+                  } else {
+                    searchedBooksShelfed.push(elSearch);
+                  }
+                }
+              });
+              count = false;
+            });
+            this.setState({
+              books: searchedBooksShelfed
+            });
+          });
+        }
+        searchedBooksShelfed = [];
+      });
+    } else {
       this.setState({
-        books: searchedbooks
+        books: []
       });
     }
   };
 
   render() {
-    const { query } = this.state;
-    if (query) {
-      BooksAPI.search(query).then(books => {
-        this.searchForBooks(books);
-      });
-    }
-
     return (
       <div>
         <div className="search-books">
           <div className="search-books-bar">
             <Link to="/">
-              <a className="close-search">Close</a>
+              <a className="close-search"> Close </a>{" "}
             </Link>
-
             <div className="search-books-input-wrapper">
+              {" "}
               {/*
-      NOTES: The search from BooksAPI is limited to a particular set of search terms.
-      You can find these search terms here:
-      https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
+              NOTES: The search from BooksAPI is limited to a particular set of search terms.
+              You can find these search terms here:
+              https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
 
-      However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-      you don't find a specific author or title. Every search is limited by search terms.
-    */}
-
-              {/* get input string from form */}
+              However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
+              you don't find a specific author or title. Every search is limited by search terms.
+            */}
               <input
                 id="search-books"
                 type="text"
                 placeholder="Search by title or author"
-                value={query}
+                value={this.state.query}
                 onChange={this.handleChange}
-              />
-            </div>
-          </div>
+              />{" "}
+            </div>{" "}
+          </div>{" "}
           <div className="search-books-results">
             <ol className="books-grid">
-              {(this.state.books.length > 0 && (
-                <BooksSlectedAfterShelf books={this.state.books} onChangeShelf = {this.props.onChangeShelf}/>
-              )) || <p> no search results </p>}
-            </ol>
-          </div>
-        </div>
+              {" "}
+              {(typeof this.state.books !== "undefined" &&
+                (this.state.books.length > 0 && (
+                  <Books
+                    books={this.state.books}
+                    onChangeShelf={this.props.onChangeShelf}
+                  />
+                ))) || <p> no search results </p>}
+            </ol>{" "}
+          </div>{" "}
+        </div>{" "}
       </div>
     );
   }
